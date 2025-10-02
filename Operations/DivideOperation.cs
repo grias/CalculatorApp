839d74c1de102bb09b3e59adce4ad422e3ad21d5
Alexander Gribowski <grias1994@gmail.com>
2025-10-02 16:24:03 +0300

    add divide-by-zero check in DivideOperation

--- Operations/DivideOperation.cs
+++ Operations/DivideOperation.cs
@@ -4,6 +4,11 @@
     {
         public double Execute(double operand1, double operand2)
         {
+            if (operand2 == 0)
+            {
+                throw new DivideByZeroException("Can't devide by zero!");
+            }
+
             return operand1 / operand2;
         }
     }
